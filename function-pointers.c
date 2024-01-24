afd8e4ff92ba696f78ff3f5196c771c1e26e27a5
David Cottrell <davidjcottrell@icloud.com>
2024-01-24 19:41:58 +0000

    add function pointer exercise

---
+++ function-pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void callbackFunc(void){
+    printf("Hello from callback function\n");
+}
+
+void actionFunc(void (*callbackFunc)(void)){
+    printf("Hello from action function\n");
+    callbackFunc();
+}
+
+int main(){
+    actionFunc(callbackFunc);
+}
